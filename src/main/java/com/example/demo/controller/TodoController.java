7c711c14036eae3837469fdabaf03bf59437ffbf
vijay <vijay.avullapati@statsncharts.com>
2020-03-09 15:05:55 +0530

    added todo component

---
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.example.demo.service.TodoService;
+
+@Controller
+//@SessionAttributes("name")
+public class TodoController {
+
+    @Autowired
+    TodoService service;
+
+    @RequestMapping(value="/list-todo", method = RequestMethod.GET)
+    public String showTodos(ModelMap model){
+        //String name = (String) model.get("name");
+        model.put("todos", service.retrieveTodos("in28Minutes"));
+        return "list-todo";
+    }
+}
--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -3,6 +3,6 @@
 <title>Yahoo!!</title>
 </head>
 <body>
-Welcome ${name} your password ${password}
+Welcome ${name}. Your <a href="/list-todo">Todo</a>
 </body>
 </html>
